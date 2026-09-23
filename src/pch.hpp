ca36c86f33a8acd50a80250500bbab3940aa421d
Marco <marco@marco007.dev>
2026-09-23 23:30:10 +0200

    fix: strip GDI/USER declarations
    CPR dependency pulls in the windows header and conflicts with raylibs
    inclusion and thus re-defines some things

--- src/pch.hpp
+++ src/pch.hpp
@@ -3,6 +3,12 @@
 #include <raymath.h>
 #include <rlgl.h>
 
+// strip conflicting GDI/USER decels out of Windows.h which CPR pulls in and conflicts with.
+#ifdef _WIN32
+#define NOGDI
+#define NOUSER
+#endif
+
 #ifdef __cplusplus
     #include <array>
     #include <mutex>
