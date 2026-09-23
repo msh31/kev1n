eb121e126515c311f2658a6ec6c0e5002de23860
marco <marco@marco007.dev>
2026-09-23 11:33:50 +0200

    feat: add mac support
    this suxx

--- src/world.cpp
+++ src/world.cpp
@@ -1,5 +1,9 @@
 #include "world.hpp"
 
+// #ifdef __APPLE__
+//     #include <raylib.h>
+// #endif
+
 void CWorld::spawn_item( int x, int y, float size, ItemType item ) {
     auto index = y * g_world_size + x;
 
@@ -23,15 +27,32 @@ bool CWorld::is_cell_blocked( int x, int y ) const {
     return true;
 }
 
+// this is stolen from SaveManager you can find it here ->
+// https://github.com/msh31/SaveManager/blob/b918bd9c6f7cea3397eeebe266270e6b98dd8e62/lib/include/utils/utils.hpp#L132
+template <typename Range, typename Fn> void enumerate( Range& range, Fn fn ) {
+#ifdef __APPLE__
+    int i = 0;
+    for ( auto& r : range ) {
+        fn( i, r );
+        ++i;
+    }
+#else
+    for ( auto [i, element] : std::views::enumerate( range ) ) {
+        fn( i, element );
+    }
+#endif
+}
+
 void CWorld::render( ) {
-    for ( auto [i, cell] : m_cells | std::views::enumerate ) {
-        if ( cell.type == ItemType::NONE ) continue;
+    enumerate( m_cells, [this]( auto i, auto& c ) {
+        if ( c.type == ItemType::NONE ) return;
+
         int x = i % g_world_size;
         int y = i / g_world_size;
 
         Color color{ };
 
-        switch ( cell.type ) {
+        switch ( c.type ) {
         case ItemType::OBSTACLE: {
             color = RED;
         } break;
@@ -46,8 +67,8 @@ void CWorld::render( ) {
         }
 
         DrawRectangle(
-            x * g_cell_pixel_size, y * g_cell_pixel_size, static_cast<int>( cell.size ), static_cast<int>( cell.size ),
+            x * g_cell_pixel_size, y * g_cell_pixel_size, static_cast<int>( c.size ), static_cast<int>( c.size ),
             color );
-        DrawRectangleLines( 0, 0, g_world_size * g_cell_pixel_size, g_world_size * g_cell_pixel_size, RED );
-    }
+    } );
+    DrawRectangleLines( 0, 0, g_world_size * g_cell_pixel_size, g_world_size * g_cell_pixel_size, RED );
 }
